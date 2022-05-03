45247febd57a176910baa2048976716135958a07
SDdylan <38325425+SDdylan@users.noreply.github.com>
2022-05-03 16:31:56 +0200

    Created UserController & UserType form Added views for the routes added Added the possibility of selection of a user role

---
+++ src/Controller/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("/users", name="user_list")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/list.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/users/create", name="user_create")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function createAction(Request $request, EntityManagerInterface $entityManager)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->encodePassword($user->getPassword());
+
+            //On attribue les rôles avec getRoles() qui permet de recupérer le ROLE_USER si il n'est pas attribué
+            $user->setRoles($user->getRoles());
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/users/{id}/edit", name="user_edit")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function editAction(int $id, UserRepository $userRepository, Request $request, EntityManagerInterface $entityManager)
+    {
+        $user = $userRepository->find($id);
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->encodePassword($user->getPassword());
+
+            //On attribue les rôles avec getRoles() qui permet de recupérer le ROLE_USER si il n'est pas attribué
+            $user->setRoles($user->getRoles());
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
